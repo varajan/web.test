edbcec12724a3a051ae3b2b526ded07b692819ae
MaksymNanyzniuk <nanymaks12@gmail.com>
2021-02-17 13:30:48 +0200

    Test branch 1: Refactoring (#1)
    * SetUp and TearDown creation
    
    * Added Waits, fixed locator for 'Login' button
    
    * Added test Button_Remind_Is_Present

--- web.test.tests/web.test.tests/LoginPageTests.cs
+++ web.test.tests/web.test.tests/LoginPageTests.cs
@@ -1,148 +1,145 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Linq;
 
 namespace web.test.tests
 {
     public class LoginPageTests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            driver.Url = "http://localhost:64177/Login";
         }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            driver.Quit();
+        }
+
 
         [Test]
         public void Empty_Login_and_Empty_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-           
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
             
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("User name and password cannot be empty!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Empty_Login_and_Wrong_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-            
-            
             driver.FindElement(By.Id("password")).SendKeys("wrong");
             
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
            
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("User name and password cannot be empty!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Empty_Login_and_Correct_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-           
             driver.FindElement(By.Id("password")).SendKeys("newyork1");
             
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
            
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("User name and password cannot be empty!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Wrong_Login_and_Empty_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-            
             driver.FindElement(By.Id("login")).SendKeys("wrong");
            
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
 
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("User name and password cannot be empty!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Wrong_Login_and_Wrong_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             string wrongUserName = "wrong";
             driver.FindElement(By.Id("login")).SendKeys(wrongUserName);
             driver.FindElement(By.Id("password")).SendKeys("wrong");
            
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
 
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("Incorrect user name!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Wrong_Login_and_Correct_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             driver.FindElement(By.Id("login")).SendKeys("wrong");
             driver.FindElement(By.Id("password")).SendKeys("newyork1");
 
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
 
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("Incorrect user name!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Correct_Login_and_Empty_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
             driver.FindElement(By.Id("login")).SendKeys("test");
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
            
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("User name and password cannot be empty!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Correct_Login_and_Wrong_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
             driver.FindElement(By.Id("login")).SendKeys("test");
             driver.FindElement(By.Id("password")).SendKeys("wrong");
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
 
             string error = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("Incorrect password!", error);
-            driver.Quit();
         }
 
         [Test]
         public void Correct_Login_and_Correct_Password()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
             driver.FindElement(By.Id("login")).SendKeys("test");
             driver.FindElement(By.Id("password")).SendKeys("newyork1");
-            driver.FindElements(By.Id("login")).Last().Click();
+            driver.FindElement(By.Id("loginBtn")).Click();
            
             Assert.IsTrue(driver.FindElement(By.Id("amount")).Displayed);
-            driver.Quit();
         }
 
+        [Test]
+        public void Button_Remind_Is_Present()
+        {
+
+            try
+            {
+                new WebDriverWait(driver, TimeSpan.FromMilliseconds(2000))
+                .Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("remindBtn")));
+            }
+            catch { }
+
+            Assert.IsTrue(driver.FindElement(By.Id("remindBtn")).Displayed);
+
+        }
     }
 }
