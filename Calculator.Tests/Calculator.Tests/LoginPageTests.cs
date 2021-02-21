67cb92dd85095b3586dcaa7c9ab444b7809258d8
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-02-21 08:01:24 +0200

    Update LoginPageTests.cs

--- Calculator.Tests/Calculator.Tests/LoginPageTests.cs
+++ Calculator.Tests/Calculator.Tests/LoginPageTests.cs
@@ -50,6 +50,98 @@ namespace Calculator.Tests
             browser.Quit();
 
         }
+        [Test]
+        public void IncorrectLoginPasswordTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys("test1");
+            browser.FindElement(By.Id("password")).SendKeys("newyork11");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("'test1' user doesn't exist!", actual);
+            browser.Quit();
+
+        }
+        [Test]
+        public void EmptyLoginPasswordTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys(" ");
+            browser.FindElement(By.Id("password")).SendKeys(" ");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("User name and password cannot be empty!", actual);
+            browser.Quit();
+
+        }
+        [Test]
+        public void EmptyLoginTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys(" ");
+            browser.FindElement(By.Id("password")).SendKeys("newyork1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("User name and password cannot be empty!", actual);
+            browser.Quit();
+
+        }
+        [Test]
+        public void EmptyPasswordTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys("test");
+            browser.FindElement(By.Id("password")).SendKeys(" ");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("User name and password cannot be empty!", actual);
+            browser.Quit();
+
+        }
+
+        [Test]
+        public void UpperCaseLoginTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys("TEST");
+            browser.FindElement(By.Id("password")).SendKeys("newyork1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("Incorrect user name!", actual);
+            browser.Quit();
+
+        }
+        [Test]
+        public void UpperCasePasswordTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys("test");
+            browser.FindElement(By.Id("password")).SendKeys("NEWYORK1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("Incorrect password!", actual);
+            browser.Quit();
+
+        }
+        [Test]
+        public void SpaceInLoginTest()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.FindElement(By.Id("login")).SendKeys(" test");
+            browser.FindElement(By.Id("password")).SendKeys("newyork1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            string actual = browser.FindElement(By.Id("errorMessage")).Text;
+            Assert.AreEqual("Incorrect user name!", actual);
+            browser.Quit();
+
+        }
 
     }
     
