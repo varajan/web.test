e5a2bf9fd09c3209e465a5c194fafa120f534a07
Maks N <nanymaks12@gmail.com>
2021-02-23 14:47:19 +0200

    Renamed 1 test: Income_And_Interest. Added 3 tests: End_Date, Financial_Year_IsClicked, Selected_Month_Name.

--- web.test.tests/web.test.tests/DepositePageTests.cs
+++ web.test.tests/web.test.tests/DepositePageTests.cs
@@ -4,7 +4,9 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Threading;
 
 namespace web.test.tests
 {
@@ -35,26 +37,20 @@ namespace web.test.tests
             driver.Quit();
         }
 
-        // Ammount
-        //Rate
-        //Term
-        //Year
-
 
         [Test]
-        public void Income_And_Interest_for_365()
+        public void Income_And_Interest()
         {
             //Arrange
             Decimal amount = 1234m; // range [0; 100,000]
             Decimal rate = 13.17m; // range [0; 100] 
-            Decimal term = 364m; // range [0; 365]
-
+            int term = 364; // range [0; 360/365]
+           
             Decimal exp_interest = Math.Round (amount * rate * term / 100 / 365, 2);
             Decimal exp_income = amount + exp_interest;
-            driver.FindElement(By.Id("d365")).Click();
-
 
             //Act
+            driver.FindElement(By.Id("d365")).Click(); // available IDs: "d360", "d365"
             driver.FindElement(By.Id("amount")).SendKeys("" + amount);
             driver.FindElement(By.Id("percent")).SendKeys("" + rate);
             driver.FindElement(By.Id("term")).SendKeys("" + term);
@@ -65,6 +61,71 @@ namespace web.test.tests
             //Assert
             Assert.AreEqual(exp_interest, act_interest);
             Assert.AreEqual(exp_income, act_income);
+        }
+
+
+        [Test]
+        public void End_Date()
+        {
+            //Arrange
+            int start_day = 1;
+            int start_month = 1;
+            int start_year = 2020;
+            int term = 364; // range [0; 360/365]
+
+            DateTime start_date = new DateTime(start_year, start_month, start_day);
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("fr-FR");
+            String exp_end_date = start_date.AddDays(term).ToString().Split(new char[] { ' ' })[0];
+
+
+            //Act
+            driver.FindElement(By.Id("d365")).Click(); // available IDs: "d360", "d365"
+
+            driver.FindElement(By.Id("term")).SendKeys("" + term);
+            
+            driver.FindElement(By.XPath("//*[@id='day']/option[" + start_day + "]")).Click();
+            driver.FindElement(By.XPath("//*[@id='month']/option[" + start_month + "]")).Click();
+            driver.FindElement(By.XPath("//*[@id='year']/option[@value='" + start_year + "']")).Click();
+
+            String act_end_date = driver.FindElement(By.Id("endDate")).GetAttribute("value");
+
+
+            //Assert
+            Assert.AreEqual(exp_end_date, act_end_date);
+        }
+
+
+        [Test]
+        public void Financial_Year_IsClicked()
+        {
+            //Arrange
+            String radio_button_id = "d365"; // available IDs: "d360", "d365"
+
+
+            //Act
+            driver.FindElement(By.Id(radio_button_id)).Click();
+
+
+            //Assert
+            Assert.AreEqual("true", driver.FindElement(By.Id(radio_button_id)).GetAttribute("checked"));
+        }
+
+
+        [Test]
+        public void Selected_Month_Name()
+        {
+            //Arrange
+            int month_number = 1;
+            string[] month_name = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            
+            
+            //Act
+            driver.FindElement(By.XPath("//*[@id='month']/option[" + month_number + "]")).Click();
+
+
+            //Assert
+            Assert.AreEqual(month_name[month_number-1], driver.FindElement(By.Id("month")).GetAttribute("value"));
         }
     }
 }
