44fd541c3ad5121f131b330c72ed1b1d6e9f3695
Aleksandr <39679039+sbfmede@users.noreply.github.com>
2021-02-25 18:27:14 +0200

    Test_URL_Verification_After_Login_Password
    Test#2

--- calculatorTest/LoginPageTest.cs
+++ calculatorTest/LoginPageTest.cs
@@ -12,20 +12,36 @@ namespace calculatorTest
         }
 
         [Test]
-        public void Test1()
+        public void Test_Incorrect_User_Login_And_Password()
         {
             IWebDriver browser = new ChromeDriver();
             browser.Url = "http://localhost:64177/Login";
             IWebElement loginfield = browser.FindElement(By.Id("login"));
             string name = "1234";
-            loginfield.SendKeys (name);
+            loginfield.SendKeys(name);
             IWebElement passwordfield = browser.FindElement(By.Id("password"));
             passwordfield.SendKeys("@#$%");
             browser.FindElements(By.Id("login"))[1].Click();
 
 
             IWebElement error = browser.FindElement(By.Id("errorMessage"));
-            Assert.AreEqual("'"+ name +"' user doesn't exist!", error.Text);
+            Assert.AreEqual("'" + name + "' user doesn't exist!", error.Text);
+            browser.Quit();
+
+
+        }
+        [Test]
+        public void Test_Incorrect_User_Login_And_Password_Incorrect_Error()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = "http://localhost:64177/Login";
+            IWebElement loginfield = browser.FindElement(By.Id("login"));
+            string name = "test";
+            loginfield.SendKeys(name);
+            IWebElement passwordfield = browser.FindElement(By.Id("password"));
+            passwordfield.SendKeys("newyork1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            Assert.AreEqual("http://localhost:64177/Deposit", browser.Url);
             browser.Quit();
         }
     }
