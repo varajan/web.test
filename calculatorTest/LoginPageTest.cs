c849473976c1da515ddd76a7a02653aaf4d026a0
Aleksandr <39679039+sbfmede@users.noreply.github.com>
2021-02-28 18:27:53 +0200

    Valid User but Password is Uppercase

--- calculatorTest/LoginPageTest.cs
+++ calculatorTest/LoginPageTest.cs
@@ -76,7 +76,7 @@ namespace calculatorTest
             browser.Quit();
         }
         [Test]
-        public void Test_User_Login_And_Password_Space_In_User()
+        public void Test_User_Login_And_Password_Space_In_User_And_Correct_Password()
         {
             IWebDriver browser = new ChromeDriver();
             browser.Url = (BASEURL + "Login");
@@ -91,7 +91,7 @@ namespace calculatorTest
             browser.Quit();
         }
         [Test]
-        public void Test_User_Login_And_Password_Negative_Tests()
+        public void Test_User_Login_And_Password_Add_t_After_User_Correct_Password()
         {
             IWebDriver browser = new ChromeDriver();
             browser.Url = (BASEURL + "Login");
@@ -104,6 +104,37 @@ namespace calculatorTest
             IWebElement error = browser.FindElement(By.Id("errorMessage"));
             Assert.AreEqual("Incorrect user name!", error.Text);
             browser.Quit();
+        }
+        [Test]
+        public void Test_User_Login_And_Password_User_Uppercase_TEST_And_Correct_Password()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = (BASEURL + "Login");
+            IWebElement loginfield = browser.FindElement(By.Id("login"));
+            string name = "TEST";
+            loginfield.SendKeys(name);
+            IWebElement passwordfield = browser.FindElement(By.Id("password"));
+            passwordfield.SendKeys("newyork1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            IWebElement error = browser.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("Incorrect user name!", error.Text);
+            browser.Quit();
+        }
+        [Test]
+        public void Test_User_Login_And_Password_Valid_User_And_Upper_Password()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Url = (BASEURL + "Login");
+            IWebElement loginfield = browser.FindElement(By.Id("login"));
+            string name = "test";
+            loginfield.SendKeys(name);
+            IWebElement passwordfield = browser.FindElement(By.Id("password"));
+            passwordfield.SendKeys("NEWYOURK1");
+            browser.FindElements(By.Id("login"))[1].Click();
+            IWebElement error = browser.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("Incorrect password!", error.Text);
+            browser.Quit();
+
         }
     }
 }
