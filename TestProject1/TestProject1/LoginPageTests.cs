9202fd84b27b007ebad2a70a6f9931c223d01d7c
Lalily77 <31096558+Lalily77@users.noreply.github.com>
2021-07-23 19:13:54 +0300

    Add SetUP and TearDown methods

--- TestProject1/TestProject1/LoginPageTests.cs
+++ TestProject1/TestProject1/LoginPageTests.cs
@@ -6,18 +6,24 @@ namespace TestProject1
 {
     public class LoginPageTests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Login";
+        }
+        [TearDown]
+        public void CleanUP() 
+        {
+            driver.Quit();
         }
 
         [Test]
         // Login with Valid login an password
         public void LoginPositiveTest()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.Id("login")).SendKeys("test");
@@ -27,16 +33,13 @@ namespace TestProject1
             // Assert
             string actualurl = driver.Url;
             Assert.AreEqual("http://localhost:64177/Deposit", actualurl);
-            driver.Quit();
+            
         }
 
         [Test]
         // Login with Empty login an password fields
         public void LoginNegativeEmptyLoginPasswordTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.XPath("//button[@id='login']")).Click();
@@ -46,16 +49,13 @@ namespace TestProject1
             string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual("User name and password cannot be empty!", actualerror);
-            driver.Quit();
+           
         }
 
         [Test]
         // Login with Valid login and Invalid password
         public void NegativeValidLoginInvalidPasswordTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.Id("login")).SendKeys("test");
@@ -67,16 +67,13 @@ namespace TestProject1
             string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual("Incorrect password!", actualerror);
-            driver.Quit();
+            
         }
 
         [Test]
         // Login with Valid password and Invalid login
         public void NegativeValidPasswordInvalidLoginTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.Id("login")).SendKeys("test1");
@@ -88,16 +85,13 @@ namespace TestProject1
             string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual("Incorrect user name!", actualerror);
-            driver.Quit();
+            
         }
 
         [Test]
-        // Login wiith Empty Login and Filled password
+        // Login with Empty Login and Filled password
         public void NegativeEmptyLoginTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.Id("password")).SendKeys("newyork1");
@@ -108,16 +102,13 @@ namespace TestProject1
             string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual("User name and password cannot be empty!", actualerror);
-            driver.Quit();
+            
         }
 
         [Test]
         // Login with Empty Password and Filled login
         public void NegativeEmptyPasswordTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.Id("login")).SendKeys("test");
@@ -128,16 +119,13 @@ namespace TestProject1
             string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual("User name and password cannot be empty!", actualerror);
-            driver.Quit();
+            
         }
 
         [Test]
         // Login with Valid password value in login field AND Valid login value in password field
         public void NegativePasswordLoginTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
             driver.FindElement(By.Id("login")).SendKeys("newyork1");
@@ -149,16 +137,13 @@ namespace TestProject1
             string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual("'newyork1' user doesn't exist!", actualerror);
-            driver.Quit();
+            
         }
 
         [Test]
         // Verify Remind Button exist
         public void RemindButtonTest()
         {
-            // Arrage
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
 
             // Act
            driver.FindElement(By.XPath("//*[@id='remind']")).Click();
@@ -166,7 +151,7 @@ namespace TestProject1
             //Assert
             string actualurl = driver.Url;
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            driver.Quit();
+           
         }
     }
 
