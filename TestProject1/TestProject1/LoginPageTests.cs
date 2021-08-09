daefe6284c957cc1c4a9d478df9dca0c03fbb453
Lalily77 <31096558+Lalily77@users.noreply.github.com>
2021-08-09 19:45:46 +0300

    Update Negative Tests

--- TestProject1/TestProject1/LoginPageTests.cs
+++ TestProject1/TestProject1/LoginPageTests.cs
@@ -18,6 +18,7 @@ namespace TestProject1
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Url = "http://localhost:64177/Login";
         }
+
         [TearDown]
         public void CleanUP() 
         {
@@ -40,26 +41,13 @@ namespace TestProject1
             
         }
 
-
-        [Test]
-        // Login with Empty login an password fields
-        public void LoginNegativeEmptyLoginPasswordTest()
-        {
-
-            // Act
-            driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
-
-            //Assert
-            string actualurl = driver.Url;
-            string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
-            Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            Assert.AreEqual("User name and password cannot be empty!", actualerror);
-           
-        }
-
         [TestCase("","", "User name and password cannot be empty!")]
+        [TestCase("", "newyork1", "User name and password cannot be empty!")]
+        [TestCase("test", "", "User name and password cannot be empty!")]
         [TestCase("test", "newyork2", "Incorrect password!")]
-        // Login with Empty login an password fields
+        [TestCase("test1", "newyork1", "Incorrect user name!")]
+        [TestCase("newyork1", "test", "'newyork1' user doesn't exist!")]
+        
         public void LoginNegativeTest(string login,string password,string error)
         {
 
@@ -74,96 +62,6 @@ namespace TestProject1
             Assert.AreEqual("http://localhost:64177/Login", actualurl);
             Assert.AreEqual(error, actualerror);
 
-        }
-
-
-
-        [Test]
-        // Login with Valid login and Invalid password
-        public void NegativeValidLoginInvalidPasswordTest()
-        {
-
-            // Act
-            driver.FindElement(By.Id("login")).SendKeys("test");
-            driver.FindElement(By.Id("password")).SendKeys("newyork2");
-            driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
-
-            //Assert
-            string actualurl = driver.Url;
-            string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
-            Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            Assert.AreEqual("Incorrect password!", actualerror);
-            
-        }
-
-        [Test]
-        // Login with Valid password and Invalid login
-        public void NegativeValidPasswordInvalidLoginTest()
-        {
-
-            // Act
-            driver.FindElement(By.Id("login")).SendKeys("test1");
-            driver.FindElement(By.Id("password")).SendKeys("newyork1");
-            driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
-
-            //Assert
-            string actualurl = driver.Url;
-            string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
-            Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            Assert.AreEqual("Incorrect user name!", actualerror);
-            
-        }
-
-        [Test]
-        // Login with Empty Login and Filled password
-        public void NegativeEmptyLoginTest()
-        {
-
-            // Act
-            driver.FindElement(By.Id("password")).SendKeys("newyork1");
-            driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
-
-            //Assert
-            string actualurl = driver.Url;
-            string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
-            Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            Assert.AreEqual("User name and password cannot be empty!", actualerror);
-            
-        }
-
-        [Test]
-        // Login with Empty Password and Filled login
-        public void NegativeEmptyPasswordTest()
-        {
-
-            // Act
-            driver.FindElement(By.Id("login")).SendKeys("test");
-            driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
-
-            //Assert
-            string actualurl = driver.Url;
-            string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
-            Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            Assert.AreEqual("User name and password cannot be empty!", actualerror);
-            
-        }
-
-        [Test]
-        // Login with Valid password value in login field AND Valid login value in password field
-        public void NegativePasswordLoginTest()
-        {
-
-            // Act
-            driver.FindElement(By.Id("login")).SendKeys("newyork1");
-            driver.FindElement(By.Id("password")).SendKeys("test");
-            driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
-
-            //Assert
-            string actualurl = driver.Url;
-            string actualerror = driver.FindElement(By.Id("errorMessage")).Text;
-            Assert.AreEqual("http://localhost:64177/Login", actualurl);
-            Assert.AreEqual("'newyork1' user doesn't exist!", actualerror);
-            
         }
 
         [Test]
