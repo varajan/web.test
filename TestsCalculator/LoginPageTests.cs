9c9ce2e3cd0b3424346de271126bb1b48ef17d29
Anna Lyakh <lYAKHANn@gmail.com>
2021-08-19 14:42:21 +0300

    add Negative test

--- TestsCalculator/LoginPageTests.cs
+++ TestsCalculator/LoginPageTests.cs
@@ -32,5 +32,29 @@ namespace TestsCalculator
             Assert.AreEqual(expectedUrl, actualUrl);
             driver.Close();
         }
+
+        [Test]
+        public void NegativeText()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Login";
+            IWebElement loginFld = driver.FindElement(By.Id("login"));
+            IWebElement passFld = driver.FindElement(By.Id("password"));
+            IWebElement loginBtn = driver.FindElements(By.Id("login"))[1];
+
+            // Act
+            loginFld.SendKeys("negativeTest");
+            passFld.SendKeys("newyork1");
+            loginBtn.Click();
+
+            // Assert
+            IWebElement errorMessage = driver.FindElement(By.Id("errorMessage"));
+            Assert.IsTrue(errorMessage.Displayed);
+            Assert.AreEqual("Incorrect user name!", errorMessage.Text);
+            driver.Close();
+        }
+
+
     }
 }
