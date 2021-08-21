45f76ecd783fb9afa3ba59c8da856d3f240c1180
Anna Lyakh <lYAKHANn@gmail.com>
2021-08-21 21:18:51 +0300

    add all tests for LoginPage

--- TestsCalculator/LoginPageTests.cs
+++ TestsCalculator/LoginPageTests.cs
@@ -25,7 +25,7 @@ namespace TestsCalculator
             loginFld.SendKeys("test");
             passFld.SendKeys("newyork1");
             loginBtn.Click();
-            
+
             // Assert
             string actualUrl = driver.Url;
             string expectedUrl = "http://localhost:64177/Deposit";
@@ -34,7 +34,7 @@ namespace TestsCalculator
         }
 
         [Test]
-        public void NegativeText()
+        public void NegativeTestWrongName()
         {
             // Arrange
             IWebDriver driver = new ChromeDriver();
@@ -55,6 +55,65 @@ namespace TestsCalculator
             driver.Close();
         }
 
+        [Test]
+        public void NegativeTestWrongPass()
+        {
+            // Arrange 
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/login";
+            IWebElement loginFld = driver.FindElement(By.Id("login"));
+            IWebElement passFld = driver.FindElement(By.Id("password"));
+            IWebElement loginBtn = driver.FindElements(By.Id("login"))[1];
 
+            // Act
+            loginFld.SendKeys("test");
+            passFld.SendKeys("test");
+            loginBtn.Click();
+
+            // Assert
+            IWebElement errorMessage = driver.FindElement(By.Id("errorMessage"));
+            Assert.IsTrue(errorMessage.Displayed);
+            Assert.AreEqual("Incorrect password!", errorMessage.Text);
+            driver.Close();
+        }
+
+        [Test]
+        public void NegativeTestWrongCredentials()
+        {
+        // Arrange
+        IWebDriver driver = new ChromeDriver();
+        driver.Url = "http://localhost:64177/login";
+        IWebElement loginFld = driver.FindElement(By.Id("login"));
+        IWebElement passFld = driver.FindElement(By.Id("password"));
+        IWebElement loginBtn = driver.FindElements(By.Id("login"))[1];
+
+        // Act
+        loginFld.SendKeys("testLogin");
+        passFld.SendKeys("testPass");
+        loginBtn.Click();
+
+        // Assert
+        IWebElement errorMessage = driver.FindElement(By.Id("errorMessage"));
+        Assert.IsTrue(errorMessage.Displayed);
+        Assert.AreEqual("\'testLogin\' user doesn\'t exist!", errorMessage.Text);
+        driver.Close();
+        }
+
+        [Test]
+        public void PositiveTestRemindPassPresent()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/login";
+            IWebElement remindBtn = driver.FindElement(By.Id("remind"));
+
+            // Act 
+            remindBtn.Click();
+
+            // Assert
+            IWebElement remindForm = driver.FindElement(By.Id("remindPasswordView"));
+            Assert.IsTrue(remindForm.Displayed);
+            driver.Close();
+        }
     }
 }
