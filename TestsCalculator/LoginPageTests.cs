4357cf75c29f90080616c9a011fcfd57cddd4730
Anna Lyakh <lYAKHANn@gmail.com>
2021-09-21 09:30:21 +0300

    add SetUp and TearDown methods

--- TestsCalculator/LoginPageTests.cs
+++ TestsCalculator/LoginPageTests.cs
@@ -8,19 +8,27 @@ namespace TestsCalculator
 {
     public class LoginPageTests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "http://127.0.0.1:8080/";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           driver.Close();
         }
 
         [Test]
         public void PositiveTest()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Url = "http://127.0.0.1:8080/";
             IWebElement loginFld = driver.FindElement(By.Id("login"));
             IWebElement passFld = driver.FindElement(By.Id("password"));
             IWebElement loginBtn = driver.FindElement(By.Id("loginBtn"));
@@ -34,17 +42,12 @@ namespace TestsCalculator
             string actualUrl = driver.Url;
             string expectedUrl = "http://127.0.0.1:8080/Deposit";
             Assert.AreEqual(expectedUrl, actualUrl);
-            driver.Close();
         }
 
         [Test]
         public void NegativeTestWrongName()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Url = "http://127.0.0.1:8080/";
             IWebElement loginFld = driver.FindElement(By.Id("login"));
             IWebElement passFld = driver.FindElement(By.Id("password"));
             IWebElement loginBtn = driver.FindElement(By.Id("loginBtn"));
@@ -58,17 +61,12 @@ namespace TestsCalculator
             IWebElement errorMessage = driver.FindElement(By.Id("errorMessage"));
             Assert.IsTrue(errorMessage.Displayed);
             Assert.AreEqual("Incorrect user name!", errorMessage.Text);
-            driver.Close();
         }
 
         [Test]
         public void NegativeTestWrongPass()
         {
             // Arrange 
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Url = "http://127.0.0.1:8080/";
             IWebElement loginFld = driver.FindElement(By.Id("login"));
             IWebElement passFld = driver.FindElement(By.Id("password"));
             IWebElement loginBtn = driver.FindElement(By.Id("loginBtn"));
@@ -82,17 +80,12 @@ namespace TestsCalculator
             IWebElement errorMessage = driver.FindElement(By.Id("errorMessage"));
             Assert.IsTrue(errorMessage.Displayed);
             Assert.AreEqual("Incorrect password!", errorMessage.Text);
-            driver.Close();
         }
 
         [Test]
         public void NegativeTestWrongCredentials()
         {
         // Arrange
-        IWebDriver driver = new ChromeDriver();
-        driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-        driver.Url = "http://127.0.0.1:8080/";
         IWebElement loginFld = driver.FindElement(By.Id("login"));
         IWebElement passFld = driver.FindElement(By.Id("password"));
         IWebElement loginBtn = driver.FindElement(By.Id("loginBtn"));
@@ -106,17 +99,12 @@ namespace TestsCalculator
         IWebElement errorMessage = driver.FindElement(By.Id("errorMessage"));
         Assert.IsTrue(errorMessage.Displayed);
         Assert.AreEqual("\'testLogin\' user doesn\'t exist!", errorMessage.Text);
-        driver.Close();
         }
 
         [Test]
         public void PositiveTestRemindPassPresent()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://127.0.0.1:8080/";
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             IWebElement remindBtn = driver.FindElement(By.Id("remindBtn"));
 
             // Act 
@@ -125,7 +113,6 @@ namespace TestsCalculator
 
             // Assert
             Assert.IsTrue(remindBtn.Displayed);
-            driver.Close();
         }
     }
 }
