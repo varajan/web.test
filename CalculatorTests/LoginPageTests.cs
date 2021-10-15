93aba6cdcbcef3ee10e5f82b509f81d5ee2c26df
Kate <kaze.no.katty@gmail.com>
2021-10-15 14:07:38 +0300

    Test project created with first test

---
+++ CalculatorTests/LoginPageTests.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CalculatorTests
+{
+    public class LoginPageTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Error_Text()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Login";
+
+            // Act
+            driver.FindElement(By.Id("login")).SendKeys("test");
+            driver.FindElement(By.Id("password")).SendKeys("password");
+            driver.FindElements(By.Id("login"))[1].Click();
+
+            // Assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("Incorrect login or password!", error.Text);
+
+            driver.Close();
+        }
+
+    }
+}
