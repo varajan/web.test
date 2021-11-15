dc29808e185586cc6858afb3fedc5efb157a9c99
Kate <kaze.no.katty@gmail.com>
2021-11-15 14:13:29 +0200

    Login tests changes

---
+++ CalculatorTests/CalculatorPageTests.cs
@@ -0,0 +1,140 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorTests
+{
+    class CalculatorPageTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void Logout()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/";
+
+            // Act
+            driver.FindElement(By.Id("login")).SendKeys("test");
+            driver.FindElement(By.Id("password")).SendKeys("newyork1");
+            driver.FindElements(By.Id("login"))[1].Click();           
+            driver.FindElement(By.XPath("/html/body/div/div/div")).Click();
+            driver.FindElement(By.XPath("/html/body/div/div/div[1]")).Click();            
+
+            // Assert
+            string currentURL = driver.Url;
+            Assert.AreEqual("http://localhost:64177/", currentURL);
+
+            driver.Close();
+        }
+        // After Logout button is removed from Settings to "http://localhost:64177/Deposit", Test need to be update
+
+        [Test]
+        public void Deposit_Texts()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Deposit";
+
+            // Act
+
+            // Assert
+            Assert.AreEqual("Deposit Amount: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[1]")).Text);
+            Assert.AreEqual("Rate of intereset: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[2]/td[1]")).Text);
+            Assert.AreEqual("Investment Term: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[3]/td[1]")).Text);
+            Assert.AreEqual("Start date: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[1]")).Text);
+            Assert.AreEqual("Financial year: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[5]/td[1]")).Text);
+            Assert.AreEqual("Income:", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[6]/th[1]")).Text);
+            Assert.AreEqual("Interest earned:", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[7]/th[1]")).Text);
+            Assert.AreEqual("End date:", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[8]/th[1]")).Text);
+            Assert.AreEqual("* - mandatory fields", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[9]/td")).Text);
+
+            driver.Close();
+        }
+
+        [Test]
+        public void Mandatory_Fields()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Deposit";
+
+            // Act
+            //driver.FindElement(By.Id("login")).SendKeys("test");
+            //driver.FindElement(By.Id("password")).SendKeys("newyork1");
+            //driver.FindElements(By.Id("login"))[1].Click();
+            //driver.FindElement(By.XPath("/html/body/div/div/div")).Click();
+            //driver.FindElement(By.XPath("/html/body/div/div/div[1]")).Click();
+
+            // Assert
+            Assert.AreEqual("Deposit Amount: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[1]/td[1]")).Text);
+            Assert.AreEqual("Rate of intereset: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[2]/td[1]")).Text);
+            Assert.AreEqual("Investment Term: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[3]/td[1]")).Text);
+            Assert.AreEqual("Start date: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[4]/td[1]")).Text);
+            Assert.AreEqual("Financial year: *", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[5]/td[1]")).Text);
+            Assert.AreEqual("Income:", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[6]/th[1]")).Text);
+            Assert.AreEqual("Interest earned:", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[7]/th[1]")).Text);
+            Assert.AreEqual("End date:", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[8]/th[1]")).Text);
+            Assert.AreEqual("* - mandatory fields", driver.FindElement(By.XPath("/html/body/div/div/table/tbody/tr[9]/td")).Text);
+
+            driver.Close();
+        }
+
+        [Test]
+        public void Logout_pageback()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Login";
+
+            // Act
+            driver.FindElement(By.Id("login")).SendKeys("test");
+            driver.FindElement(By.Id("password")).SendKeys("newyork1");
+            driver.FindElements(By.Id("login"))[1].Click();
+            driver.FindElement(By.LinkText("Settings")).Click();
+            driver.FindElement(By.LinkText("Logout")).Click();
+
+            // Assert
+            string currentURL = driver.Url;
+            Assert.AreEqual("http://localhost:64177/Deposit", currentURL);
+
+
+            driver.Close();
+        }
+
+        [Test]
+        public void CalculateTest()
+        {
+            // Arrange
+            // Income = Amount/100*Rate * Term/FinYear
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Login";
+
+            // Act
+            driver.FindElement(By.Id("login")).SendKeys("test");
+            driver.FindElement(By.Id("password")).SendKeys("newyork1");
+            driver.FindElements(By.Id("login"))[1].Click();
+            driver.FindElement(By.Id("amount")).SendKeys("1000");
+            driver.FindElement(By.Id("percent")).SendKeys("25");
+            driver.FindElement(By.Id("term")).SendKeys("365");
+            driver.FindElement(By.Id("d365")).Click();
+            string income = driver.FindElement(By.Id("income")).GetAttribute("value");
+            string interest = driver.FindElement(By.Id("interest")).GetAttribute("value");
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("1250,00", income);
+                Assert.AreEqual("250,00", interest);
+            });
+
+            driver.Close();
+        }
+    }
+}
--- CalculatorTests/LoginPageTests.cs
+++ CalculatorTests/LoginPageTests.cs
@@ -7,18 +7,25 @@ using System.Threading;
 namespace CalculatorTests
 {
     public class LoginPageTests
-    { 
+    {
+        private IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void OpenLoginPage()
         {
+            driver = new ChromeDriver();
+            driver.Url = "http://localhost:64177/Login";
         }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
         [Test]
         public void Login_Label_Text()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             // driver.FindElement(By.ClassName("user"));
 
@@ -27,83 +34,49 @@ namespace CalculatorTests
             IWebElement passwordFieldText = driver.FindElement(By.ClassName("pass"));
             Assert.AreEqual("Password:", passwordFieldText.Text);
             Assert.AreEqual("User:", userFieldText.Text);
-
-
-            driver.Close();
         }
 
-
-        [Test]
-        public void Failed_Login_Error_Text()
+        [TestCase("test", "password", "Incorrect login or password!")]
+        [TestCase("", "", "User name and password cannot be empty!")]
+        [TestCase("", "newyork1", "User name and password cannot be empty!")]
+        [TestCase("test", "", "User name and password cannot be empty!")]
+        [TestCase("test1", "newyork", "'test1' user doesn't exist!")] //Login_Not_Existing_Account
+        public void Failed_Login_Error_Texts(string login, string password, string expectedError)
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
-            driver.FindElement(By.Id("login")).SendKeys("test");
-            driver.FindElement(By.Id("password")).SendKeys("password");
+            driver.FindElement(By.Id("login")).SendKeys(login);
+            driver.FindElement(By.Id("password")).SendKeys(password);
             driver.FindElements(By.Id("login"))[1].Click();
 
             // Assert
             IWebElement error = driver.FindElement(By.Id("errorMessage"));
-            Assert.AreEqual("Incorrect login or password!", error.Text);
+            Assert.AreEqual(expectedError, error.Text);
 
-            driver.Close();
             //I won't dublicate test for incorrect login
             //It will be good to use iterations for one test with different parameters
         }
 
-        [Test]
-        public void Login_Empty_Fields_Error_Text()
-        {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
+        //[Test]
+        //public void Failed_Login_Attempts_Error()
+        //{
+        //    // Act
+        //    driver.FindElement(By.Id("login")).SendKeys("test");
+        //    driver.FindElement(By.Id("password")).SendKeys("password");
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        driver.FindElements(By.Id("login"))[1].Click();
+        //    }
+        //    // Assert
+        //    IWebElement error = driver.FindElement(By.Id("errorMessage"));
+        //    Assert.AreEqual("Failed logins more then 5 times. Please wait 15 min for next try.", error.Text);
 
-            // Act
-            driver.FindElement(By.Id("login")).SendKeys("");
-            driver.FindElement(By.Id("password")).SendKeys("");
-            driver.FindElements(By.Id("login"))[1].Click();
-
-            // Assert
-            IWebElement error = driver.FindElement(By.Id("errorMessage"));
-            Assert.AreEqual("User name and password cannot be empty!", error.Text);
-
-            driver.Close();
-            //Same, won't dublicate test for empty login or password
-
-        }
-
-        [Test]
-        public void Failed_Login_Attempts_Error()
-        {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
-            // Act
-            driver.FindElement(By.Id("login")).SendKeys("test");
-            driver.FindElement(By.Id("password")).SendKeys("password");
-            for (int i = 0; i < 5; i++)
-            {
-                driver.FindElements(By.Id("login"))[1].Click();
-            }
-            // Assert
-            IWebElement error = driver.FindElement(By.Id("errorMessage"));
-            Assert.AreEqual("Failed logins more then 5 times. Please wait 15 min for next try.", error.Text);
-
-            driver.Close();
-            // it may be necessary to add a check that the user has not logged in
-        }
+        //    driver.Close();
+        //    // it may be necessary to add a check that the user has not logged in
+        //}
 
         [Test]
         public void Success_Login()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             driver.FindElement(By.Id("login")).SendKeys("test");
             driver.FindElement(By.Id("password")).SendKeys("newyork1");
@@ -112,18 +85,11 @@ namespace CalculatorTests
             // Assert
             string currentURL = driver.Url;
             Assert.AreEqual("http://localhost:64177/Deposit", currentURL);
-
-
-            driver.Close();
         }
 
         [Test]
         public void Login_Not_Case_Sensitive()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             driver.FindElement(By.Id("login")).SendKeys("Test");
             driver.FindElement(By.Id("password")).SendKeys("newyork1");
@@ -133,69 +99,23 @@ namespace CalculatorTests
             string currentURL = driver.Url;
             Assert.AreEqual("http://localhost:64177/Deposit", currentURL);
 
-
-            driver.Close();
         } // login should not be case sensitive (this is in practice)
-
-        [Test]
-        public void Login_Not_Existing_Account()
-        {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
-            // Act
-            string login = "Test1";
-            driver.FindElement(By.Id("login")).SendKeys(login);
-            driver.FindElement(By.Id("password")).SendKeys("newyork");
-            driver.FindElements(By.Id("login"))[1].Click();
-
-            // Assert           
-            IWebElement error = driver.FindElement(By.Id("errorMessage"));
-            Assert.AreEqual($"'{login}' user doesn't exist!", error.Text);
-
-            driver.Close();
-        }
 
         [Test]
         public void Login_Buttons_Exist()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             bool  remindBtn = driver.FindElement(By.Id("remind")).Displayed;
             bool loginBtn = driver.FindElements(By.Id("login"))[1].Displayed;
 
             // Assert
-            Assert.AreEqual(true, remindBtn, "button is not displayed");
-            Assert.AreEqual(true, loginBtn);
-
-            driver.Close();
-
-            //It seems it works but if  completely remove an element from the code, there is an exception,
-            //I want to use some clear message?
-            //I tried to apply try-catch, but using it the test passes
-            //even if there is no element at all in the code, how I can display that the test failed?
-            //try
-            //{
-            //    bool remindBtn = driver.FindElement(By.Id("remind")).Displayed;
-            //    bool loginBtn = driver.FindElements(By.Id("login"))[1].Displayed;
-            //}
-            //catch (OpenQA.Selenium.NoSuchElementException)
-            //{
-            //    System.Diagnostics.Debug.WriteLine("Element was not found");
-            //}
+            Assert.AreEqual(true, remindBtn, "button is not displayed");  
+            Assert.IsTrue(loginBtn); // = Assert.AreEqual(true, loginBtn);
         }
 
         [Test]
         public void Remind_Password_Success()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             driver.FindElement(By.Id("remind")).Click();            
             _ = driver.SwitchTo().Frame("remindPasswordView");
@@ -207,17 +127,11 @@ namespace CalculatorTests
             // Assert
             Assert.AreEqual($"Email with instructions was sent to {playerEmail}", alertText);         
             driver.SwitchTo().Alert().Accept();
-
-            driver.Close();
         }
 
         [Test]
         public void Remind_Password_Validation_Email()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             driver.FindElement(By.Id("remind")).Click();
             driver.SwitchTo().Frame("remindPasswordView");
@@ -228,16 +142,11 @@ namespace CalculatorTests
             // Assert
             Assert.AreEqual("Invalid email", errorText);
 
-            driver.Close();
         } //Also here I would like to add some iterations to different email validations, so as not to write a lot of code
 
         [Test]
         public void Remind_Password_Email_Not_Exist()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             driver.FindElement(By.Id("remind")).Click();
             driver.SwitchTo().Frame("remindPasswordView");
@@ -247,17 +156,11 @@ namespace CalculatorTests
 
             // Assert
             Assert.AreEqual("No user was found", errorText);
-
-            driver.Close();
         }
 
         [Test]
         public void Remind_Password_Empty_Form()
         {
-            // Arrange
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "http://localhost:64177/Login";
-
             // Act
             driver.FindElement(By.Id("remind")).Click();
             driver.SwitchTo().Frame("remindPasswordView");
@@ -272,7 +175,6 @@ namespace CalculatorTests
             // Assert
             Assert.AreEqual("", email);
 
-            driver.Close();
         }//After sending the email, need to clear the iframe from the address and message
     }
 }
