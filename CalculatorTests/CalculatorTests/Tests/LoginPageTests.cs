9f27fa2b5b29d6cd443117b581f77ecca72f665e
marinaprozorovska <maryna.paska97@gmail.com>
2022-05-03 10:51:04 +0300

    Created 10 auto tests

--- CalculatorTests/CalculatorTests/Tests/LoginPageTests.cs
+++ CalculatorTests/CalculatorTests/Tests/LoginPageTests.cs
@@ -30,7 +30,7 @@ namespace CalculatorTests.Tests
         }
 
         [Test]
-        public void LoginWithEmptyPasswordTest()
+        public void ValidLoginWithEmptyPasswordTest()
         {
             // arrange
             var options = new ChromeOptions { AcceptInsecureCertificates = true };
@@ -41,7 +41,7 @@ namespace CalculatorTests.Tests
             IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
 
             // act
-            loginField.SendKeys("Test");
+            loginField.SendKeys("test");
             loginButton.Click();
             Thread.Sleep(500);
 
@@ -51,5 +51,204 @@ namespace CalculatorTests.Tests
 
             driver.Quit();
         }
+
+        [Test]
+        public void InvalidLoginWithEmptyPasswordTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement loginField = driver.FindElements(By.Id("login"))[0];
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            loginField.SendKeys("12345");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("User not found!", error.Text);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void ValidLoginWithEmptyLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            passwordField.SendKeys("newyork1");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("Incorrect user name!", error.Text);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void InvalidLoginWithEmptyLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            passwordField.SendKeys("67890");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("User not found!", error.Text);
+
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void InvalidLoginAndInvalidPasswordLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement loginField = driver.FindElements(By.Id("login"))[0];
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            loginField.SendKeys("12345");
+            passwordField.SendKeys("67890");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("User not found!", error.Text);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void ValidLoginAndInvalidPasswordLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement loginField = driver.FindElements(By.Id("login"))[0];
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            loginField.SendKeys("test");
+            passwordField.SendKeys("67890");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("Incorrect password!", error.Text);
+
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void InvalidLoginAndValidPasswordLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement loginField = driver.FindElements(By.Id("login"))[0];
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            loginField.SendKeys("12345");
+            passwordField.SendKeys("newyork1");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMesage"));
+            Assert.AreEqual("Incorrect user name!", error.Text);
+
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void ValidLoginAndValidPasswordLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement loginField = driver.FindElements(By.Id("login"))[0];
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            loginField.SendKeys("test");
+            passwordField.SendKeys("newyork1");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement success = driver.FindElement(By.Id("calculateBtn"));
+            Assert.AreEqual("Calculate", success.Text);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void MixedValidLoginAndValidPasswordLoginTest()
+        {
+            // arrange
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001";
+
+            IWebElement loginField = driver.FindElements(By.Id("login"))[0];
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElements(By.Id("login"))[1];
+
+            // act
+            loginField.SendKeys("newyork1");
+            passwordField.SendKeys("test");
+            loginButton.Click();
+            Thread.Sleep(500);
+
+            // assert
+            IWebElement error = driver.FindElement(By.Id("errorMessage"));
+            Assert.AreEqual("User not found!", error.Text);
+
+            driver.Quit();
+        }
     }
+
 }
