53e423b97f530bab664762f81ac64dbf2e40851f
larazayec <lara.zayec@gmail.com>
2022-05-09 22:34:48 +0300

    Labal Password

--- Test2/Tests/LoginPageTests.cs
+++ Test2/Tests/LoginPageTests.cs
@@ -69,24 +69,7 @@ namespace Test2.Tests
             Assert.AreEqual(exepted, error.Text);
             driver.Quit();
         }
-        [Test]
-        public void NewUser()
-        {
-            var options = new ChromeOptions { AcceptInsecureCertificates = true };
-            IWebDriver driver = new ChromeDriver(options);
-            driver.Url = "https://localhost:5001/";
-            IWebElement loginFld = driver.FindElement(By.Id("login"));
-            IWebElement passwFld = driver.FindElement(By.Id("password"));
-            IWebElement loginBut = driver.FindElements(By.Id("login"))[1];
-            loginFld.SendKeys("Alex");
-            passwFld.SendKeys("12345678");
-            loginBut.Click();
-            Thread.Sleep(500);
-            string ActualUrl = driver.Url;
-            string expectedUrl = "https://localhost:5001/Calculator";
-            Assert.AreEqual(expectedUrl, ActualUrl);
-            driver.Quit();
-        }
+        
         [Test]
         public void NegativLoginTest()
         {
@@ -96,7 +79,7 @@ namespace Test2.Tests
             IWebElement loginFld = driver.FindElement(By.Id("login"));
             IWebElement passwFld = driver.FindElement(By.Id("password"));
             IWebElement LoginBut = driver.FindElements(By.Id("login"))[1];
-            loginFld.SendKeys("Test");
+            loginFld.SendKeys("set");
             passwFld.SendKeys("newyork1");
             LoginBut.Click();
             Thread.Sleep(500);
@@ -122,6 +105,19 @@ namespace Test2.Tests
             string expectedUrl = "https://localhost:5001/Calculator";
             Assert.AreEqual(expectedUrl, ActualUrl);
             driver.Quit();            
+        }
+
+        [Test]
+        public void LabelPass()
+        {
+            var options = new ChromeOptions { AcceptInsecureCertificates = true};
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001/";
+            IWebElement labPass = driver.FindElement(By.ClassName("pass"));
+            Thread.Sleep(600);
+            string expected = "Password";
+            Assert.AreEqual(expected, labPass.Text);
+            driver.Quit();
         }
     }
 }
